02aee1f93a5f0874d0be7d8562b18a54ed0d98c1
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-02-01 02:09:38 -0300

    #5 - Modelar a API de vendas - feature/sp1/#5

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }        
+        public double Price { get; set; }        
+        public string Image { get; set; }
+        public int Stock { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool Active { get; set; }
+        public SaleItem SaleItem { get; private set; }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/SaleItem.cs
@@ -0,0 +1,32 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using System.Diagnostics;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class SaleItem : BaseEntity
+    {
+        public Guid SaleId { get; private set; }
+        public Guid ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal Price { get; private set; }
+        public string ProductImage { get; set; }
+
+        // EF Rel.
+        public Sale Sale { get; set; }
+        public Product Product { get; set; }
+
+        public SaleItem(Guid productId, string productName, int quantity,
+                        decimal price, string productImage = null)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Quantity = quantity;
+            Price = price;
+            ProductImage = productImage;
+        }
+
+        // EF ctor
+        protected SaleItem() { }
+    }
+}
