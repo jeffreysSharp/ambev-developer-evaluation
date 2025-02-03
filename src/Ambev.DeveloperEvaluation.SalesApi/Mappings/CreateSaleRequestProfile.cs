4c34ed194904cd3a89f154c58767146dbeb697c3
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-02-03 11:28:30 -0300

    #15 - Criar endpoint de Sales - feature/sp1/#15

---
+++ src/Ambev.DeveloperEvaluation.SalesApi/Mappings/CreateSaleRequestProfile.cs
@@ -0,0 +1,13 @@
+﻿using Ambev.DeveloperEvaluation.SalesApi.Features.Sale.CreateSale;
+using Ambev.SalesApi.Application.Sales.CreateSale;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.SalesApi.Mappings;
+
+public class CreateSaleRequestProfile : Profile
+{
+    public CreateSaleRequestProfile()
+    {
+        CreateMap<CreateSaleRequest, CreateSaleCommand>();
+    }
+}
